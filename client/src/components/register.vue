1fc057a22c367346484b24684d394a1419ef4837
kentezrabetita <kentezrabetita@gmail.com>
2021-02-02 22:00:22 +0800

    client register route

---
+++ client/src/components/register.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 class="btn btn-primary">Register Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'register'
+}
+</script>
+
+<style>
+
+</style>
